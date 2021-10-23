cc70b61d3e497a951515762c9e0659dd7c980f09
AlexanderRod <alexander.rodrigo.calderon@gmail.com>
2021-10-22 23:56:13 -0500

    primercommit

--- delivery/web/urls.py
+++ delivery/web/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = 'web'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('producto/<int:producto_id>/', views.producto, name='producto'),
+    path('categoria/<int:categoria_id>/', views.productoPorCategory, name='categoria'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
+    path('carrito',views.carrito,name='carrito'),
+    path('eliminarProductoCarrito/<int:producto_id>',views.eliminarProductoCarrito,name="eliminarProductoCarrito"),
+    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito')
 ]
